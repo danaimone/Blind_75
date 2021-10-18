3ba550d227a74b3b35d9df90d082f4c3facd27da
Dan Aimone <30274286+danaimone@users.noreply.github.com>
2021-10-17 21:25:44 -0700

    added time complexity notes

--- Graphs/alien-dictionary/alien-dictionary.py
+++ Graphs/alien-dictionary/alien-dictionary.py
@@ -1,5 +1,11 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
+        '''
+        Time Complexity: O(c) for every character relation
+        Space Complexity: O(1) every relation must be represented and no additional space is used.
+        :param words: list of words in lexicographic order
+        :return: lexicographically ascending alien characters
+        '''
         adj_list = {c: [] for word in words for c in word}
         for first_word, second_word in zip(words, words[1:]):
             for first_char, second_char in zip(first_word, second_word):
