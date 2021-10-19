34c69613e7deb7edfd2f22fc1cbc39432b3646a1
Dan Aimone <30274286+danaimone@users.noreply.github.com>
2021-10-18 18:41:34 -0700

    Time: 39 ms (34.77%), Space: 14.2 MB (43.18%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+
+        memo = [0 for _ in range(n)]
+        memo[0] = 1 # this is acting as our
+        memo[1] = 2
+        for i in range(2, n):
+            memo[i] = memo[i-1] + memo[i-2]
+        return memo[-1]
+            
